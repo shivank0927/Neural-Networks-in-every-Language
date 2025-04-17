4e95250094068d1cba81923053fbabbce036aeb8
shivank0927 <shivang.arya09@gmail.com>
2025-04-17 09:03:55 +0530

    model created

--- lua/nn.lua
+++ lua/nn.lua
@@ -6,21 +6,28 @@ local HIDDEN_NODES = 2
 local OUTPUTS = 1
 local TRAINING = 4
 
-local function init()
+local function initWeight() -- random weight initialization
 	return math.random()
 end
 
 print("random weight", init())
 
-local function sigmoid(x)
+local function sigmoid(x) -- sigmoid function
 	return (1 / (1 + math.exp(-x)))
 end
 
-local function derivative(x)
+local function derivative(x) -- signoid derivative
 	return (x * (1 - x))
 end
 
-local function main()
+local function shuffle(table) -- shuffle data
+	for i = #table, 2, -1 do
+		local j = math.random(1, i)
+		table[i], table[j] = table[j], table[i]
+	end
+end
+
+local function initModel() -- main function
 	local rate = 0.1
 
 	local hiddenLayer = {}
@@ -61,4 +68,62 @@ local function main()
 	local trainingInputs = { { 0.0, 0.0 }, { 1.0, 0.0 }, { 0.0, 1.0 }, { 1.0, 1.0 } }
 
 	local trainingOutputs = { { 0.0 }, { 1.0 }, { 1.0 }, { 0.0 } }
+
+	for i = 0, INPUTS - 1 do
+		for j = 1, HIDDEN_NODES - 1 do
+			hiddenWeights[i][j] = initWeight() -- initialize random input weights
+		end
+	end
+
+	for i = 0, HIDDEN_NODES - 1 do
+		for j = 1, OUTPUTS - 1 do
+			outputWeights[i][j] = initWeight() -- initialize random output weights
+		end
+	end
+
+	for i = 0, OUTPUTS - 1 do -- output layer bias
+		outputLayerBias[i] = initWeight()
+	end
+
+	local trainingSetOrder = { 0, 1, 2, 3 }
+	local numberOfEpochs = 10000
+end
+
+local function hiddenLayerActivation(trainingSetOrder, trainingInputs, hiddenLayer, hiddenLayerBias)
+	for x = 1, #trainingSetOrder do
+		local i = trainingSetOrder[x]
+
+		-- Hidden layer activation computation
+		for j = 1, #HIDDEN_NODES do
+			local activation = hiddenLayerBias[j]
+
+			for k = 1, #trainingInputs[i] do
+				activation = activation + trainingInputs[i][k]
+			end
+
+			hiddenLayer[j] = sigmoid(activation)
+		end
+	end
+end
+
+local function outputLayerActivation(trainingSetOrder, trainingInputs, outputLayer, outputLayerBias, outputWeights)
+	for j = 1, #OUTPUTS do
+		activation = outputLayerBias[j]
+
+		for k = 1, #HIDDEN_NODES do
+			activation = outputLayer[k] * outputWeights[k][j]
+		end
+		outputLayer[j] = sigmoid(activation)
+	end
+end
+
+local function epochRepresent(numberOfEpochs, trainingSetOrder)
+	for epoch = 0, #numberOfEpochs do
+		shuffle(trainingSetOrder)
+		for x = 1, #trainingSetOrder do
+			local i = trainingSetOrder[x]
+			hiddenLayerActivation()
+			outputLayerActivation()
+		end
+	end
 end
